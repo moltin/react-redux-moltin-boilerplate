304098c528656deb6e2776056703b891b4ac96ac
Jamie Barton <hi_jamie@me.com>
2017-10-01 18:41:02 +0100

    Use redux bindActionCreators inside HomeContainer

--- src/components/Home/HomeContainer.js
+++ src/components/Home/HomeContainer.js
@@ -1,47 +1,25 @@
 import React, {Component} from 'react';
+import {bindActionCreators} from 'redux';
 import {connect} from 'react-redux';
 import {GetProducts} from '../../ducks/products';
 import {GetCollections} from '../../ducks/collections';
 import {GetCategories} from '../../ducks/categories';
 
-function mapStateToProps(state) {
-  return state;
-}
-
 class HomeContainer extends Component {
-  // a react lifecycle event, read more at http://busypeoples.github.io/post/react-component-lifecycle/
   componentDidMount() {
-    const boundGetProducts = () => {
-      this.props.dispatch(dispatch => {
-        dispatch(GetProducts('files, main_images, collections'));
-      });
-    };
-
-    const boundGetCategories = () => {
-      this.props.dispatch(dispatch => {
-        dispatch(GetCategories());
-      });
-    };
-
-    const boundGetCollections = () => {
-      this.props.dispatch(dispatch => {
-        dispatch(GetCollections());
-      });
-    };
-
     // check if we already have a moltin products in the store
     if (this.props.products.fetched === false) {
-      boundGetProducts();
+      this.props.GetProducts('files, main_images, collections');
     }
 
     // now we do the same thing for categories
     if (this.props.categories.fetched === false) {
-      boundGetCategories();
+      this.props.GetCategories();
     }
 
     // then collections
     if (this.props.collections.fetched === false) {
-      boundGetCollections();
+      this.props.GetCollections();
     }
   }
 
@@ -58,4 +36,20 @@ class HomeContainer extends Component {
   }
 }
 
-export default connect(mapStateToProps)(HomeContainer);
+const mapDispatchToProps = dispatch =>
+  bindActionCreators(
+    {
+      GetCollections,
+      GetCategories,
+      GetProducts
+    },
+    dispatch
+  );
+
+const mapStateToProps = ({collections, categories, products}) => ({
+  collections,
+  categories,
+  products
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(HomeContainer);
